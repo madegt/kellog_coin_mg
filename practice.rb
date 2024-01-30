b0e63b74ffdb3f42951f7e63d3e64765f783f835
Madeleine Garbin <madeleine.garbin@kellogg.northwestern.edu>
2024-01-30 04:16:27 +0000

    extrawork

--- practice.rb
+++ practice.rb
@@ -17,6 +17,57 @@ blockchain = [
   { "from_user" => "anthony", "to_user" => "evan", "amount" => 1750 }
 ]
 
+#start from zero
+total_from_ben=0
+total_from_anthony=0
+total_from_brian=0
+total_from_evan=0
+total_to_ben=0
+total_to_anthony=0
+total_to_brian=0
+total_to_evan=0
+
+# Sum all from_users 
+blockchain.each do |conditional_sum|
+  total_from_ben += conditional_sum["amount"] if conditional_sum["from_user"] == "ben"
+end
+blockchain.each do |conditional_sum|
+  total_from_brian += conditional_sum["amount"] if conditional_sum["from_user"] == "brian"
+end
+blockchain.each do |conditional_sum|
+  total_from_anthony += conditional_sum["amount"] if conditional_sum["from_user"] == "anthony"
+end
+blockchain.each do |conditional_sum|
+  total_from_evan += conditional_sum["amount"] if conditional_sum["from_user"] == "evan"
+end
+
+# Sum all to_users 
+blockchain.each do |conditional_sum|
+  total_to_ben += conditional_sum["amount"] if conditional_sum["to_user"] == "ben"
+end
+blockchain.each do |conditional_sum|
+  total_to_brian += conditional_sum["amount"] if conditional_sum["to_user"] == "brian"
+end
+blockchain.each do |conditional_sum|
+  total_to_anthony += conditional_sum["amount"] if conditional_sum["to_user"] == "anthony"
+end
+blockchain.each do |conditional_sum|
+  total_to_evan += conditional_sum["amount"] if conditional_sum["to_user"] == "evan"
+end
+
+#net amounts = to_users - from_users
+
+total_ben=total_to_ben-total_from_ben
+total_brian=total_to_brian-total_from_brian
+total_anthony=total_to_anthony-total_from_anthony
+total_evan=total_to_evan-total_from_evan
+
+puts "Ben's KelloggCoin balance is #{total_ben}"
+puts "Brians's KelloggCoin balance is #{total_brian}"
+puts "Evans's KelloggCoin balance is #{total_evan}"
+puts "Anthony's KelloggCoin balance is #{total_anthony}"
+
+
 # Write code below that returns the number of KelloggCoin that each user has in their 
 # KelloggCoin "wallet".
 
